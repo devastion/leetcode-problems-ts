a6eca737ee7a4fbdfd74d26b667fbb0dc906d888
Dimitar Banev <banev_dimitar@mail.com>
2023-04-06 23:58:19 +0300

    problem: 1. two sum

---
+++ src/1_two_sum/twoSum.test.ts
@@ -0,0 +1,19 @@
+import { twoSum } from "./twoSum";
+
+describe("twoSum tests", () => {
+  test("test case 1", () => {
+    expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+  });
+
+  test("test case 2", () => {
+    expect(twoSum([3, 2, 4], 6)).toEqual([1, 2]);
+  });
+
+  test("test case 3", () => {
+    expect(twoSum([3, 3], 6)).toEqual([0, 1]);
+  });
+
+  test("test case 4", () => {
+    expect(twoSum([-1, -2, -3, -4, -5], -8)).toEqual([2, 4]);
+  });
+});
--- src/index.test.ts
+++
@@ -1,5 +0,0 @@
-import { add } from ".";
-
-test("two plus two is four", () => {
-  expect(add(2, 3)).toBe(5);
-});

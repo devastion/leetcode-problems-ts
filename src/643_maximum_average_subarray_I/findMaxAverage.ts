3316c4375bbef4505513a0f2be0f363d993edf0e
Dimitar Banev <banev_dimitar@mail.com>
2023-04-11 00:20:15 +0300

    problem(643): add description

--- src/643_maximum_average_subarray_I/findMaxAverage.ts
+++ src/643_maximum_average_subarray_I/findMaxAverage.ts
@@ -1,3 +1,11 @@
+/**
+ * You are given an integer array nums consisting of n
+ * elements, and an integer k.
+ * Find a contiguous subarray whose length is equal to k
+ * that has the maximum average value and return this value.
+ * Any answer with a calculation error less than 10-5 will be accepted.
+ */
+
 export const findMaxAverage = (nums: number[], k: number): number => {
   let maxSum = 0;
   let tempSum = 0;
